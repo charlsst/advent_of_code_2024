4fb99c097789cdd93e75266fac340a7168ce12e5
charlsst <charles@tiedeman.co.uk>
2024-12-04 15:23:09 +0000

    fixed day 3

--- Day3/corruptFix.py
+++ Day3/corruptFix.py
@@ -6,15 +6,12 @@ tempLines = corruptionFile.readlines()
 corruptionLines = []
 
 if part2 :
-    for line in tempLines :
-        line.replace("don't()", "do()GETRIDOFME")
-        for i in line.split("do()") :
-            if i[1][:10] != "GETRIDOFME" :
-                corruptionLines.append(i)
+    line = ''.join(tempLines).replace("don't()", "do()GETRIDOFME")
+    for i in line.split("do()") :
+        if i[:10] != "GETRIDOFME" :
+            corruptionLines.append(i)
 else :
     corruptionLines = tempLines
-
-for i in corruptionLines : print(i)
 
 mulInstances = []
 for line in corruptionLines :
@@ -22,11 +19,6 @@ for line in corruptionLines :
         mulIndex = line.find('mul(')
         mulInstances.append(line[mulIndex:mulIndex+12])
         line = line.replace('mul(', '', 1)
-
-#for i in corruptionLines : print(f"- {i}\n")
-
-print("HELLO"[:4])
-    
 
 total = 0
 for possible in mulInstances :
@@ -36,18 +28,8 @@ for possible in mulInstances :
         possibleList = possible.split(',')
         if len(possibleList) == 2 and possibleList[0].find(' ') == -1 and possibleList[1].find(' ') == -1 :
             total += int(possibleList[0]) * int(possibleList[1])
-            print(f"{possibleList[0]} * {possibleList[1]} = ({int(possibleList[0]) * int(possibleList[1])})")
     except : pass
 
 print(f"Total: {total}")
 
 corruptionFile.close()
-
-#*187194524 - part 1
-
-# 374378353
-#*187194524
-# 128670117
-# 112945167
-# 109108832
-# 44026963
